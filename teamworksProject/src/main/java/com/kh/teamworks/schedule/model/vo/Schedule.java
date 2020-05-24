8b07200728bc23f2988cb1d7c841116433ad90a9
JISU <63631066+JISUUCHOI@users.noreply.github.com>
2020-05-24 15:19:45 +0900

    schedule lombok test

--- teamworksProject/src/main/java/com/kh/teamworks/schedule/model/vo/Schedule.java
+++ teamworksProject/src/main/java/com/kh/teamworks/schedule/model/vo/Schedule.java
@@ -1,5 +1,25 @@
 package com.kh.teamworks.schedule.model.vo;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class Schedule {
+	
+	private int scheduleNo;
+	private String scheduleCategory;
+	private String empId;
+	private String scheduleTitle;
+	private String scheduleContent;
+	private String startDate;
+	private String endDate;
+	private String deleteStatus;
 
 }
