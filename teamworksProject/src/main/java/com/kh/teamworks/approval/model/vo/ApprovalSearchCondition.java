85ad3801f2fda52bdeeb6a7d2edb88bb15f335df
choi-hs <59041755+choi-hs@users.noreply.github.com>
2020-05-30 08:10:56 +0900

    수정

--- teamworksProject/src/main/java/com/kh/teamworks/approval/model/vo/ApprovalSearchCondition.java
+++ teamworksProject/src/main/java/com/kh/teamworks/approval/model/vo/ApprovalSearchCondition.java
@@ -17,4 +17,11 @@ public class ApprovalSearchCondition {
 	private String deptName;
 	private String jobName;
 	
+	private int docNo;				// 문서코드
+	private String empId;			// 사원번호
+	private String docSc;			// 문서양식명		// 기안서, 경조사, 휴가, 제증명
+	private int docStatus;			// 문서상태		// 0:대기 1: 진행 2: 완료 3: 반려 4:회수
+	private String docTitle;		// 문서제목
+	private String docReference;	// 참조자
+	private String docDate;			// 기안일자
 }
