b68df76d2d3b40f269f7d8ca5d5898ed74825cbe
NNIIE <greg0917@naver.com>
2020-06-01 04:31:12 +0900

    수정

--- teamworksProject/src/main/java/com/kh/teamworks/approval/model/service/ApprovalService.java
+++ teamworksProject/src/main/java/com/kh/teamworks/approval/model/service/ApprovalService.java
@@ -1,10 +1,27 @@
 package com.kh.teamworks.approval.model.service;
 
+import java.util.ArrayList;
+
+import com.kh.teamworks.approval.model.vo.ApproveSearchCondition;
 import com.kh.teamworks.approval.model.vo.Document;
+import com.kh.teamworks.approval.model.vo.Document;
+import com.kh.teamworks.employee.model.vo.Employee;
+
 
 public interface ApprovalService {
 
 	// 제증명 신청
 	int insertProof(Document d);
+	
+	// 1_1. 문서 작성 전, 화면에 보여 줄 기본 사원정보(사원명, 소속부서명) select
+	Employee selectEmpInfo(String empId);
+	
+	// 1_2. 참조자 조직도 부서 select용
+	ArrayList<Employee> selectDeptName();
+	
+	// 1_3. 참조자 조직도 사원 select용
+	ArrayList<Employee> selectOrgChart();
 
+	// 2. 결재선/참조자 사원 검색 select
+	ArrayList<Employee> selectEmpSch(ApproveSearchCondition sc);
 }
