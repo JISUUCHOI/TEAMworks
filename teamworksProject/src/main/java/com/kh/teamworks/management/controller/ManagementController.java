600eae6b68265c34a7a9dda179031b56bd57ae25
ymkim9302 <63789951+ymkim9302@users.noreply.github.com>
2020-06-01 19:09:26 +0900

    work

--- teamworksProject/src/main/java/com/kh/teamworks/management/controller/ManagementController.java
+++ teamworksProject/src/main/java/com/kh/teamworks/management/controller/ManagementController.java
@@ -10,6 +10,8 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.kh.teamworks.management.model.service.ManagementService;
 
@@ -18,5 +20,13 @@ public class ManagementController {
 	
 	@Autowired
 	private ManagementService mgService;
+	
+	@RequestMapping("companyInfo.mg")
+	public String selectCompanyInfo(Model model){
+		
+		CompanyInfo = 
+	}
+	
+	
 	
 }
