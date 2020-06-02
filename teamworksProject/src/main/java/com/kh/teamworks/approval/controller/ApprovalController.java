12282f54e8a7ff5b27c1c133d65db4ed1ecfda32
NNIIE <greg0917@naver.com>
2020-06-02 21:34:20 +0900

    드래그앤드롭

--- teamworksProject/src/main/java/com/kh/teamworks/approval/controller/ApprovalController.java
+++ teamworksProject/src/main/java/com/kh/teamworks/approval/controller/ApprovalController.java
@@ -7,7 +7,9 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Iterator;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -16,9 +18,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import com.google.gson.Gson;
 import com.kh.teamworks.approval.model.service.ApprovalService;
@@ -111,7 +115,8 @@ public class ApprovalController {
 							  @RequestParam(name="uploadFile", required=false) MultipartFile file) {
 		
 		 System.out.println(d);
-				
+			
+
 
 		 
 		 int result = aService.insertDraft(d);
@@ -122,6 +127,7 @@ public class ApprovalController {
 	    }else {
 	    	return "common/errorPage";
 	    }
+	}
 	
 	// 결재선 insert
 	
@@ -133,9 +139,57 @@ public class ApprovalController {
 	// 기안문insert
 	// @RequestMapping("draftInsert.ap")
 	// public String insertDraft
-	
-	
-	
-	
-	}
+	   
+	    @RequestMapping(value = "/fileUpload", method = RequestMethod.GET)
+	    public String dragAndDrop(Model model) {
+	        
+	        return "fileUpload";
+	        
+	    }
+	    
+	    @RequestMapping(value = "/fileUpload/post") //ajax에서 호출하는 부분
+	    @ResponseBody
+	    public String upload(MultipartHttpServletRequest multipartRequest) { //Multipart로 받는다.
+	         
+	        Iterator<String> itr =  multipartRequest.getFileNames();
+	        
+	        String filePath = "C:/test"; //설정파일로 뺀다.
+	        
+	        while (itr.hasNext()) { //받은 파일들을 모두 돌린다.
+	            
+	            /* 기존 주석처리
+	            MultipartFile mpf = multipartRequest.getFile(itr.next());
+	            String originFileName = mpf.getOriginalFilename();
+	            System.out.println("FILE_INFO: "+originFileName); //받은 파일 리스트 출력'
+	            */
+	            
+	            MultipartFile mpf = multipartRequest.getFile(itr.next());
+	     
+	            String originalFilename = mpf.getOriginalFilename(); //파일명
+	     
+	            String fileFullPath = filePath+"/"+originalFilename; //파일 전체 경로
+	     
+	            try {
+	                //파일 저장
+	                mpf.transferTo(new File(fileFullPath)); //파일저장 실제로는 service에서 처리
+	                
+	                System.out.println("originalFilename => "+originalFilename);
+	                System.out.println("fileFullPath => "+fileFullPath);
+	     
+	            } catch (Exception e) {
+	                System.out.println("postTempFile_ERROR======>"+fileFullPath);
+	                e.printStackTrace();
+	            }
+	                         
+	       }
+	         
+	        return "success";
+	    }
+	    
+	    @RequestMapping("drag.ap")
+	    public String enrollForm()
+	    {
+	        return "approval/drag";
+	    }
+	    
 }
