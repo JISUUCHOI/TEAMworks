d2ef900eff19b777500510bbef01305596268ce8
NNIIE <58434352+NNIIE@users.noreply.github.com>
2020-06-15 00:10:57 +0900

    vo 수정

--- teamworksProject/src/main/java/com/kh/teamworks/approval/model/vo/DocumentList.java
+++ teamworksProject/src/main/java/com/kh/teamworks/approval/model/vo/DocumentList.java
@@ -14,7 +14,7 @@ import lombok.ToString;
 public class DocumentList {
 	
 	private String empId;
-	private int dlNo;
+	private String dlNo;
 	private String dlForm;
 	private String dlName;
 	private int dlLike;
